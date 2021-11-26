0a9c740a4b29cd08e79ebe3fa8eb2c3dfea51f2b
DvPresto <devin.m.preston@gmail.com>
2021-11-26 14:43:33 -0500

    Set url to Week 12 NFL projections. Call axios to use that url.

--- web-scraper/index.js
+++ web-scraper/index.js
@@ -5,4 +5,29 @@ const express = require('express')
 
 const app = express()
 
+//assuming Week 12
+const url = 'https://fantasy.nfl.com/research/projections?position=O&sort=projectedPts&statCategory=projectedStats&statSeason=2021&statType=weekProjectedStats&statWeek=12'
+
+axios(url)
+
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
+//url bank
+//https://fantasy.nfl.com/research/projections?position=1&statCategory=projectedStats&statSeason=2021&statType=weekProjectedStats&statWeek=12
+//position mapping
+// 1 QB
+// 2 RB
+// 3 WR
+// 4 TE
+// 5 WR + RB?
+// 6 ???
+// 7 K
+// 8 DEF
+// O All Offense (letter O) 
+//
+// Further pages:
+//https://fantasy.nfl.com/research/projections?offset=26&position=O&sort=projectedPts&statCategory=projectedStats&statSeason=2021&statType=weekProjectedStats&statWeek=12
+//
+//QBs = https://fantasy.nfl.com/research/projections?offset=26&position=1&sort=projectedPts&statCategory=projectedStats&statSeason=2021&statType=weekProjectedStats&statWeek=12
+//"offset=x"
